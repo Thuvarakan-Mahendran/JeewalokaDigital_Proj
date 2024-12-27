f596fe440728ad68c251837978865af7a188af06
dulan126 <167549912+dulan126@users.noreply.github.com>
2024-12-27 11:35:50 +0530

    first

---
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/JeewalokadigitalApplication.java
@@ -0,0 +1,13 @@
+package com.jeewaloka.digital.jeewalokadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeewalokadigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeewalokadigitalApplication.class, args);
+	}
+
+}
---
+++ Backend/jeewalokadigital/jeewalokadigital/src/test/java/com/jeewaloka/digital/jeewalokadigital/JeewalokadigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeewaloka.digital.jeewalokadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeewalokadigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
