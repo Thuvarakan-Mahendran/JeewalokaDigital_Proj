22ee613a9a89495a61e2030a3b7d3bf8bf77ab69
MS7-dust-in-universe <thuvarakan12403@usci.ruh.ac.lk>
2025-01-25 11:27:28 +0530

    update with user tasks

--- Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/service/UserService.java
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/service/UserService.java
@@ -1,7 +1,16 @@
 package com.jeewaloka.digital.jeewalokadigital.service;
 
+import com.jeewaloka.digital.jeewalokadigital.dto.UserDTO;
 import com.jeewaloka.digital.jeewalokadigital.entity.User;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public interface UserService{
-    User findByRole(String role);
+    List<User> findByRole(String role);
+    List<User> searchByTerm(String searchTerm);
+    void deleteByUserID(Long userid);
+    List<User> addUsers(List<User> users);
 }
