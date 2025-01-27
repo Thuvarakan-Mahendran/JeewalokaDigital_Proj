f45f31e8b36526782261a1b0c7312b62b1bdcea0
janithsandaruwan <sandaruwan12421@usci.ruh.ac.lk>
2025-01-27 22:23:30 +0530

    Create GRNItem.java

---
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/entity/GRNItem.java
@@ -0,0 +1,43 @@
+package com.jeewaloka.digital.jeewalokadigital.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class GRNItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long grnItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "grn_id", nullable = false)
+    private GRN grn;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double unitPrice;
+
+    @Column(nullable = false)
+    private Double totalAmount;
+
+    @Column(nullable = false)
+    private LocalDate itemExpiryDate;
+
+    @Column(nullable = false)
+    private LocalDate itemManufactureDate;
+}
