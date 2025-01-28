d9183bf8a8a1f664e7d4e4fb890ea87d29fb6d00
dulan126 <167549912+dulan126@users.noreply.github.com>
2025-01-28 11:09:51 +0530

    POM.xml changed. Spring version changed

--- Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/controller/BillController.java
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/controller/BillController.java
@@ -1,3 +1,4 @@
+
 package com.jeewaloka.digital.jeewalokadigital.controller;
 
 import com.jeewaloka.digital.jeewalokadigital.dto.BillDTO;
@@ -28,3 +29,4 @@ public class BillController {
         return responseEntity;
     }
 }
+
--- Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/entity/Retailer.java
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/entity/Retailer.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Table(name = "seller")
+@Table(name = "retailer")
 public class Retailer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
