72aa1ae59108ecf6570616c11ece40e7861c9e8c
janithsandaruwan <sandaruwan12421@usci.ruh.ac.lk>
2025-02-05 08:29:59 +0530

    Create GRNItemDTO.java

---
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/dto/Request/GRNItemDTO.java
@@ -0,0 +1,18 @@
+package com.jeewaloka.digital.jeewalokadigital.dto.Request;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class GRNItemDTO {
+    private Long grnItemId;
+    private Long itemId;
+    private Integer quantity;
+    private Double unitPrice;
+    private Double totalAmount;
+    private LocalDate itemExpiryDate;
+    private LocalDate itemManufactureDate;
+
+
+}
