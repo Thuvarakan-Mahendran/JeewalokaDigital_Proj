322ec3561c865557b1adfd7268333337dba01931
janithsandaruwan <sandaruwan12421@usci.ruh.ac.lk>
2025-02-06 11:24:05 +0530

    Update index.jsx

--- FrontEnd/Jeewaloka/src/routes/index.jsx
+++ FrontEnd/Jeewaloka/src/routes/index.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Items from "../pages/Items/Items";
+import GRN from "../pages/GRN/GRN";
 import Dashboard from "../layouts/Layout";
+import PurchaseOrder from "../pages/PurchaseOrder/PurchaseOrder";
 import "../styles/index.css";
 
 const AppRoutes = () => {
@@ -9,6 +11,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Dashboard />}>
           <Route path="inventary/items" element={<Items />} />
+          <Route path="inventary/grn" element={<GRN />} />
+          <Route path="inventary/purchaseorder" element={<PurchaseOrder />} />
         </Route>
       </Routes>
     </BrowserRouter>
