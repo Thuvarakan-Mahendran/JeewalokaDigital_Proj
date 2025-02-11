ed05750d9d74814fe49b296e9b7a3af55dcb1242
janithsandaruwan <sandaruwan12421@usci.ruh.ac.lk>
2025-02-11 23:34:51 +0530

    Update SupplierReturnedNote.java

--- Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/entity/SupplierReturns/SupplierReturnedNote.java
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/entity/SupplierReturns/SupplierReturnedNote.java
@@ -1,30 +1,30 @@
-package com.jeewaloka.digital.jeewalokadigital.entity.SupplierReturns;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Table(name = "SupplierReturnedNote")
-public class SupplierReturnedNote {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sbillId", nullable = false, length = 10)
-    private int sbillId;
-
-    @Column(name = "date", nullable = false, length = 10)
-    private Date date;
-
-    @Column(name = "ReturnTotal", nullable = false, length = 10)
-    private long ReturnTotal;
-
-    @Column(name = "ClearedTotal", nullable = false, length = 10)
-    private long ClearedTotal;
-}
+//package com.jeewaloka.digital.jeewalokadigital.entity.SupplierReturns;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.Date;
+//
+//@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//@Table(name = "SupplierReturnedNote")
+//public class SupplierReturnedNote {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "sbillId", nullable = false, length = 10)
+//    private int sbillId;
+//
+//    @Column(name = "date", nullable = false, length = 10)
+//    private Date date;
+//
+//    @Column(name = "ReturnTotal", nullable = false, length = 10)
+//    private long ReturnTotal;
+//
+//    @Column(name = "ClearedTotal", nullable = false, length = 10)
+//    private long ClearedTotal;
+//}
