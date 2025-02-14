990d5be5bd94a81d55d8f1cacee891a547d42fa4
dulan126 <167549912+dulan126@users.noreply.github.com>
2025-02-14 23:09:12 +0530

    First frontend commit by dulan

--- FrontEnd/Jeewaloka/src/routes/index.jsx
+++ FrontEnd/Jeewaloka/src/routes/index.jsx
@@ -3,6 +3,7 @@ import Items from "../pages/Items/Items";
 import GRN from "../pages/GRN/GRN";
 import Dashboard from "../layouts/Layout";
 import PurchaseOrder from "../pages/PurchaseOrder/PurchaseOrder";
+import Sellers from "../pages/Sellers/Sellers";
 import "../styles/index.css";
 
 const AppRoutes = () => {
@@ -13,6 +14,7 @@ const AppRoutes = () => {
           <Route path="inventary/items" element={<Items />} />
           <Route path="inventary/grn" element={<GRN />} />
           <Route path="inventary/purchaseorder" element={<PurchaseOrder />} />
+          <Route path="sales/sellers" element = {<Sellers />}/>
         </Route>
       </Routes>
     </BrowserRouter>
