7d8a3895af12d32baf6a7452f3f9cac0b20d0120
janithsandaruwan <sandaruwan12421@usci.ruh.ac.lk>
2025-02-18 14:52:25 +0530

    Update GRNItemResponseDTO.java

--- Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/dto/Response/GRNItemResponseDTO.java
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/dto/Response/GRNItemResponseDTO.java
@@ -2,20 +2,16 @@ package com.jeewaloka.digital.jeewalokadigital.dto.Response;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDate;
 
 @Setter
 @Getter
 public class GRNItemResponseDTO {
-    // Getters and Setters
     private Long itemId;
     private String itemName;
     private String itemCode;
     private Integer quantity;
     private Double unitPrice;
-    private Double totalAmount;
     private LocalDate itemExpiryDate;
     private LocalDate itemManufactureDate;
-
 }
