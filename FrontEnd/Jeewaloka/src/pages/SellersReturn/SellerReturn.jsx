aebd7bc2f0804f7f228d71480473f0f7c7a4fba1
Munasinghe-1 <priyanidumunasinghe012@gmail.com>
2025-02-18 20:46:38 +0530

    Create SellerReturn.jsx

---
+++ FrontEnd/Jeewaloka/src/pages/SellersReturn/SellerReturn.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function SellerReturn() {
+  return (
+    <div className="p-6 bg-gray-50 min-h-screen">
+        <h2 className='text-2xl font-semibold text-gray-800'>Seller Returns</h2>
+        <div className="bg-white p-4 shadow rounded-lg flex justify-between mb-4 items-center">
+            <input type='text' placeholder='Search...' className="w-1/3 rounded border border-gray-300 py-2 px-5"/>
+        </div>
+        <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">Create Return Note</button><br/><br/>
+        <div className="bg-white p-4 shadow rounded-lg">
+            <table className="w-full text-left border-collapse">
+            <thead>
+                <tr>
+                    <th>Return Id</th>
+                    <th>Seller Id</th>
+                    <th>SellerName</th>
+                    <th>ReturnDate</th>
+                    <th>ACTIONS</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
