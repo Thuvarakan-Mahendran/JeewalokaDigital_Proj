67d6c4c8f7ed4c604f7d1175e42bdeb8813f4197
Munasinghe-1 <priyanidumunasinghe012@gmail.com>
2025-02-18 20:46:41 +0530

    Update index.jsx

--- FrontEnd/Jeewaloka/src/routes/index.jsx
+++ FrontEnd/Jeewaloka/src/routes/index.jsx
@@ -3,6 +3,9 @@ import Items from "../pages/Items/Items";
 import GRN from "../pages/GRN/GRN";
 import Dashboard from "../layouts/Layout";
 import PurchaseOrder from "../pages/PurchaseOrder/PurchaseOrder";
+import GRRN from "../pages/GRRN/GRRN";
+import SellerOrder from "../pages/SellerOrder/SellerOrder";
+import SellerReturn from "../pages/SellersReturn/SellerReturn";
 import "../styles/index.css";
 
 const AppRoutes = () => {
@@ -13,6 +16,9 @@ const AppRoutes = () => {
           <Route path="inventary/items" element={<Items />} />
           <Route path="inventary/grn" element={<GRN />} />
           <Route path="inventary/purchaseorder" element={<PurchaseOrder />} />
+          <Route path="inventary/grrn" element={<GRRN />} />
+          <Route path="sales/sellerorder" element={<SellerOrder/>} />
+          <Route path="sales/sellersreturns" element={<SellerReturn/>} />
         </Route>
       </Routes>
     </BrowserRouter>
