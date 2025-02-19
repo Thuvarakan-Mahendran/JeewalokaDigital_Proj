169d7c3fcf109910ced78e3716349693184d4cf3
janithsandaruwan <sandaruwan12421@usci.ruh.ac.lk>
2025-02-19 19:13:31 +0530

    Update index.jsx

--- FrontEnd/Jeewaloka/src/routes/index.jsx
+++ FrontEnd/Jeewaloka/src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Items from "../pages/Items/Items";
 import GRN from "../pages/GRN/GRN";
 import Dashboard from "../layouts/Layout";
@@ -6,20 +6,32 @@ import PurchaseOrder from "../pages/PurchaseOrder/PurchaseOrder";
 import Sellers from "../pages/Sellers/Sellers";
 import Supplier from "../pages/Supplier/Supplier";
 import "../styles/index.css";
+import LoginPage from "../pages/Login/LoginForm";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />}>
+        {/* Redirect '/' to '/login' */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Login Page */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Protected Dashboard Route */}
+        <Route path="/dashboard" element={<Dashboard />}>
           <Route path="inventary/items" element={<Items />} />
           <Route path="inventary/grn" element={<GRN />} />
           <Route path="inventary/purchaseorder" element={<PurchaseOrder />} />
-          <Route path="sales/sellers" element = {<Sellers />}/>
-          <Route path="inventary/supplier" element={<Supplier/>} />
+          <Route path="sales/sellers" element={<Sellers />} />
+          <Route path="inventary/supplier" element={<Supplier />} />
         </Route>
+
+      
+
       </Routes>
     </BrowserRouter>
   );
-}
+};
+
 export default AppRoutes;
