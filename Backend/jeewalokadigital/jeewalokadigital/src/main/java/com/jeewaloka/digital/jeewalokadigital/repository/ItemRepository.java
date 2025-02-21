0a3fb916550d33bd834f6dac0d959e27978e6eee
janithsandaruwan <sandaruwan12421@usci.ruh.ac.lk>
2025-02-21 08:52:20 +0530

    Update ItemRepository.java

--- Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/repository/ItemRepository.java
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/repository/ItemRepository.java
@@ -1,9 +1,17 @@
 package com.jeewaloka.digital.jeewalokadigital.repository;
 
+import com.jeewaloka.digital.jeewalokadigital.entity.GRN;
 import com.jeewaloka.digital.jeewalokadigital.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item,Long> {
-}
+
+//        @Query("SELECT MAX(i.itemCode2) FROM Item i WHERE i.itemCode2 LIKE 'IT-%'")
+//        String findLastItemCode2();
+    }
+
