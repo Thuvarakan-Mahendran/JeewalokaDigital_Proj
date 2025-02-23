304e27ae256f32aa4b087aa74f8545fa14b02aaa
dulan126 <167549912+dulan126@users.noreply.github.com>
2025-02-23 08:22:03 +0530

    backend update by dulan

--- Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/mapper/BillMapper.java
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/mapper/BillMapper.java
@@ -44,7 +44,7 @@ public class BillMapper {
 
         // Fetch and set Retailer by ID
         if (billDTO.getRetailerID() != null) {
-            Retailer retailer = retailerRepo.findById(String.valueOf(billDTO.getRetailerID()))
+            Retailer retailer = retailerRepo.findById(Math.toIntExact(billDTO.getRetailerID()))
                     .orElseThrow(() -> new EntityNotFoundException("Retailer not found with ID: " + billDTO.getRetailerID()));
             bill.setRetailer(retailer);
         }
