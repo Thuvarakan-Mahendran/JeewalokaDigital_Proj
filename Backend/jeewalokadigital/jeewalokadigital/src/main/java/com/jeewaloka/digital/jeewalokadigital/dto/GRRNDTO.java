6962956d74719ba4a981dc2e42d6bf19a6777b84
Gavin Vinsara <gavinvinsaramax@gmail.com>
2025-02-24 22:10:51 +0530

    Create GRRNDTO.java

---
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/dto/GRRNDTO.java
@@ -0,0 +1,19 @@
+package com.jeewaloka.digital.jeewalokadigital.dto;
+
+import com.jeewaloka.digital.jeewalokadigital.entity.GRRNItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GRRNDTO {
+    private Long grrnid;
+    private String supplierName;
+    private LocalDate returneDate;
+    private List<GRRNItem> grrnitemId;
+}
