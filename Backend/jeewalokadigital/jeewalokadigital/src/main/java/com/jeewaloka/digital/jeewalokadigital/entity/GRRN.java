cf76cb3fcab5fa0e609d5974d1a5f76209b5123b
Gavin Vinsara <gavinvinsaramax@gmail.com>
2025-02-24 22:11:25 +0530

    Create GRRN.java

---
+++ Backend/jeewalokadigital/jeewalokadigital/src/main/java/com/jeewaloka/digital/jeewalokadigital/entity/GRRN.java
@@ -0,0 +1,35 @@
+package com.jeewaloka.digital.jeewalokadigital.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Good_Return_Note")
+public class GRRN {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long grrnid;
+
+    @Column(nullable = false)
+    private String supplierName;
+
+    @Column(nullable = false)
+    private LocalDate returneDate;
+
+
+    @OneToMany(mappedBy = "grrnitemId", cascade = CascadeType.ALL)
+    private List<GRRNItem> grrnitemId;
+
+
+
+
+}
